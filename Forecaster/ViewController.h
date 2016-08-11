27fa7d296477e2c005039c2b29cf66f5599b5093
Ross Gottschalk <rossgottschalk@yahoo.com>
2016-08-11 10:03:03 -0400

    Initial Commit

---
+++ Forecaster/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Forecaster
+//
+//  Created by Ross Gottschalk on 8/11/16.
+//  Copyright © 2016 The Iron Yard. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
